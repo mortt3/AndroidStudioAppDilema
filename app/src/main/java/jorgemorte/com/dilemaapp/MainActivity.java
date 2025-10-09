557902b4518fffa3ca376589588f3e3555e4001e
mortt3 <jorgemortecampos@gmail.com>
2025-10-09 22:53:07 +0200

    Crear db funcional simple

---
+++ app/src/main/java/jorgemorte/com/dilemaapp/MainActivity.java
@@ -0,0 +1,10 @@
+package jorgemorte.com.dilemaapp;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+}
